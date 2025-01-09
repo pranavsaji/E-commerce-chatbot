6d929f329a2722b8b4261a218d9cd351dd93d01d
pranavsaji <pranavs9876@gmail.com>
2025-01-09 17:59:02 -0600

    new commit

--- orderapp.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from order_endpoints import router
-
-app = FastAPI(title="Order Dataset API", description="API for querying order-related data")
-
-# Include endpoints
-app.include_router(router)
